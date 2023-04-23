c3cf4732745b03496a7f0650b188b51708c2639f
iShqfty <76799463+iShqfty@users.noreply.github.com>
2023-04-23 19:59:34 -0400

    Update CExample.h

--- SDK/CExample.h
+++ SDK/CExample.h
@@ -2,7 +2,7 @@
 
 #include "SRC.h"
 
-inline constexpr auto CEXAMPLE_EXAMPLECALL_ADDR = 0XDE4DB33F;
+inline constexpr auto CEXAMPLE_EXAMPLECALL_ADDR = 0XDE4DB33F; //Not actual RVA
 
 struct CExample {
 	//example: remodel Player_o* class as CExample class
